5811f6bcdede1f1e736405892b498d6db9481645
JerryChen <cn_jerrychen@qq.com>
2019-08-29 15:57:57 +0800

    add index and bootstrap

--- src/main/java/com/jerrychen/dome/controller/IndexController.java
+++ src/main/java/com/jerrychen/dome/controller/IndexController.java
@@ -5,20 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
-public class HelloController {
+public class IndexController {
+    @GetMapping("/")
+    String index(){return "index";}
 
 
-
-
-
-
-
-
-        @GetMapping("/hello")
-        public String hello(@RequestParam(name="name") String name, Model model) {
-            model.addAttribute("name", name);
-            return "hello";
-        }
 
     }
 
