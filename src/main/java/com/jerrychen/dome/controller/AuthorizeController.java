ec3d8a8528d71f23ea1cffe81cb6731528f35366
jerrychen <cn_jerrychen@qq.com>
2019-09-04 17:44:47 +0800

    add

--- src/main/java/com/jerrychen/dome/controller/AuthorizeController.java
+++ src/main/java/com/jerrychen/dome/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.jerrychen.dome.controller;
 
 import com.jerrychen.dome.dto.AccessTokenDTO;
 import com.jerrychen.dome.dto.GithubUser;
+import com.jerrychen.dome.mapper.UserMapper;
+import com.jerrychen.dome.model.User;
 import com.jerrychen.dome.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
     @Value("${github.client.secret}")
     private  String clientSecret;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     String callback(@RequestParam(name="code")String code,
                     @RequestParam(name="state")String state,
@@ -40,10 +46,17 @@ public class AuthorizeController {
 
        String accessToken= githubProvider.getAccessToken(accessTokenDTO);
         System.out.println(accessToken);
-        GithubUser user=githubProvider.getUser(accessToken);
-        if (null!=user){
+        GithubUser githubUser=githubProvider.getUser(accessToken);
+        if (null!=githubUser){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功
-            request.getSession().setAttribute("user",user);
+            request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
 
 
---
+++ src/main/java/com/jerrychen/dome/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.jerrychen.dome.mapper;
+
+import com.jerrychen.dome.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) value(#{name},#{accountId},#{token}#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
