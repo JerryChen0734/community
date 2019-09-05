9c785af13cb9e89c5888619fb50e5b335ae12caf
jerrychen <cn_jerrychen@qq.com>
2019-09-05 11:18:23 +0800

    change sql

--- src/main/java/com/jerrychen/dome/mapper/UserMapper.java
+++ src/main/java/com/jerrychen/dome/mapper/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) value(#{name},#{accountId},#{token}#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) value(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 }
