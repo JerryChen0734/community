a9089eb41d97d734a379d228f56366015393738c
JerryChen <cn_jerrychen@qq.com>
2019-09-23 10:03:11 +0800

    feat(comment model):add function of the comment

---
+++ src/main/java/com/jerrychen/community/controller/CommentController.java
@@ -0,0 +1,39 @@
+package com.jerrychen.community.controller;
+
+
+import com.jerrychen.community.dto.CommentDTO;
+import com.jerrychen.community.mapper.CommentMapper;
+import com.jerrychen.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by codedrinker on 2019/5/30.
+ */
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO) {
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
